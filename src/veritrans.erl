50a4c8b11fc6b5a1991aa89a5f55d420a4d2b539
Taufan Aditya <toopay@taufanaditya.com>
2015-09-29 13:16:36 +0700

    Low levels API

--- src/veritrans.erl
+++ src/veritrans.erl
@@ -1,15 +1,32 @@
 -module(veritrans).
 -behaviour(gen_server).
 
+%% API
 -export([start_link/0, start/0, stop/0]).
+-export([
+    set_key/1,
+    set_flag/1,
+    charge/1,
+    capture/1,
+    approve/1,
+    cancel/1,
+    status/1,
+    expire/1
+]).
+
+%% Gen server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {finished = false, vendor = undefined}).
+%% Constant and record
+-record(state, {key = <<"undefined">>, flag = sandbox}).
+-define(DEFAULT_TIMEOUT, 60000).
 
+%% Include generic headers (datatype and endpoint constants)
 -include("veritrans.hrl").
 
+
 %%====================================================================
-%% External functions
+%% API
 %%====================================================================
 %% @doc Starts the veritrans process linked to the calling process. Usually invoked by the supervisor veritrans_sup
 %% @spec start_link() -> {ok, pid()}
@@ -22,15 +39,49 @@ start() ->
 
 %% @doc Stop the veritrans process. 
 stop() ->
-    case catch gen_server:call(veritrans, stop) of
+    case catch gen_server:call(?MODULE, stop) of
         {'EXIT',{noproc,_}} ->
             ok;
         Res ->
             Res
     end.
 
+%% @doc Set the server key
+set_key(Key) ->
+    gen_server:cast(?MODULE, {change_key, Key}), %% Async call to set the current key
+    ok.
+
+%% @doc Set the server flag
+set_flag(Flag) ->
+    gen_server:cast(?MODULE, {change_flag, Flag}), %% Async call to set the current flag
+    ok.
+
+%% @doc Do the charge
+charge(Data) ->
+    gen_server:call(?MODULE, {post, ?CHARGE_ENDPOINT, Data},?DEFAULT_TIMEOUT).
+
+%% @doc Capture authorized transaction
+capture(Data) ->
+    gen_server:call(?MODULE, {post, ?CAPTURE_ENDPOINT, Data},?DEFAULT_TIMEOUT).
+
+%% @doc Get a transaction status
+status(TransactionId) ->
+    gen_server:call(?MODULE, {get_with_placeholder, ?STATUS_SUFFIX_ENDPOINT, get_binary(TransactionId)},?DEFAULT_TIMEOUT).
+
+%% @doc Approve a pending transaction
+approve(TransactionId) ->
+    gen_server:call(?MODULE, {post_with_placeholder, ?APPROVE_SUFFIX_ENDPOINT, get_binary(TransactionId)},?DEFAULT_TIMEOUT).
+
+%% @doc Cancel a pending transaction
+cancel(TransactionId) ->
+    gen_server:call(?MODULE, {post_with_placeholder, ?CANCEL_SUFFIX_ENDPOINT, get_binary(TransactionId)},?DEFAULT_TIMEOUT).
+
+%% @doc Expires a pending transaction
+expire(TransactionId) ->
+    gen_server:call(?MODULE, {post_with_placeholder, ?EXPIRE_SUFFIX_ENDPOINT, get_binary(TransactionId)},?DEFAULT_TIMEOUT).
+
 %%====================================================================
-%% Server functions
+%% gen_server callbacks
 %%====================================================================
 %%--------------------------------------------------------------------
 %% Function: init/1
@@ -54,9 +105,27 @@ init(_) ->
 %%          {stop, Reason, Reply, State}   | (terminate/2 is called)
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
-handle_call({do_charge, Url, Params}, _From, State) ->
-	io:format("Charging... Url:~p~nParams:~p~nState:~p~n", [Url, Params, State]),
-    {reply, Params, State};
+handle_call({post, Endpoint, Params}, _From, State) ->
+    Url = build_url(State#state.flag, Endpoint),
+    Reply = case ibrowse:send_req(get_list(Url), [], post, jsx:encode(Params), build_options(State#state.key)) of
+        {ok, _Status, _Head, Body} -> {ok, get_data(Body)};
+        {error, Reason} -> {error, get_binary(Reason)}
+    end,
+    {reply, Reply, State};
+handle_call({post_with_placeholder, Endpoint, Param}, _From, State) ->
+    Url = build_url_with_prefix(State#state.flag, Endpoint, Param),
+    Reply = case ibrowse:send_req(get_list(Url), [], post, [], build_options(State#state.key)) of
+        {ok, _Status, _Head, Body} -> {ok, get_data(Body)};
+        {error, Reason} -> {error, get_binary(Reason)}
+    end,
+    {reply, Reply, State};
+handle_call({get_with_placeholder, Endpoint, Param}, _From, State) ->
+    Url = build_url_with_prefix(State#state.flag, Endpoint, Param),
+    Reply = case ibrowse:send_req(get_list(Url), [], get, [], build_options(State#state.key)) of
+        {ok, _Status, _Head, Body} -> {ok, get_data(Body)};
+        {error, Reason} -> {error, get_binary(Reason)}
+    end,
+    {reply, Reply, State};
 handle_call(stop, _From, State) ->
     {stop, normal, ok, State}.
 
@@ -67,6 +136,10 @@ handle_call(stop, _From, State) ->
 %%          {noreply, State, Timeout} |
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
+handle_cast({change_key, Key}, State) ->
+    {noreply, State#state{key=Key}};
+handle_cast({change_flag, Flag}, State) ->
+    {noreply, State#state{flag=Flag}};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -95,3 +168,44 @@ terminate(_Reason, _State) ->
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%%====================================================================
+%% Internal function
+%%====================================================================
+%% @doc Generate a url
+build_url(Flag, Endpoint) ->
+    Root = case Flag of
+        live -> ?LIVE_ROOT;
+        _ -> ?SANDBOX_ROOT
+    end,
+    <<Root/binary,Endpoint/binary>>.
+
+%% @doc Generate a url with prefix
+build_url_with_prefix(Flag, Endpoint, Prefix) ->
+    build_url(Flag, <<"/",Prefix/binary,Endpoint/binary>>).
+
+%% @doc Generates options
+build_options(Key) ->
+    [{basic_auth,{get_list(Key), ""}}].
+
+%% @doc Get the data
+get_data(MaybeJson) ->
+    try
+        jsx:decode(get_binary(MaybeJson))
+    catch _:_ ->
+        get_binary(MaybeJson)
+    end.
+
+%% @doc Get the list representation of either binary or list
+get_list(Binary) when is_binary(Binary)-> binary_to_list(Binary);
+get_list(List) -> List.
+
+%% @doc Get the binary representation of either undefined, integer, list, tuple or atom
+get_binary(undefined) -> <<>>;
+get_binary(Atom) when is_atom(Atom)-> list_to_binary(atom_to_list(Atom));
+get_binary(Binary) when is_binary(Binary)-> Binary;
+get_binary(Integer) when is_integer(Integer)-> integer_to_binary(Integer);
+get_binary(List) when is_list(List)-> list_to_binary(List);
+get_binary(Tuple) when is_tuple(Tuple) ->
+    {_, Val} = Tuple,
+    get_binary(Val).
